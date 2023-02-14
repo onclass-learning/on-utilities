d6b0a870fdfbfcc1072497d2b8a6ef3ecbb719c4
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2023-02-14 08:35:55 -0300

    feat: format bytes

---
+++ tests/format-bytes.test.js
@@ -0,0 +1,10 @@
+import { formatBytes } from '../src/format-bytes';
+
+describe('formatBytes', () => {
+  it('formatBytes correctly formats input', () => {
+    expect(formatBytes(0)).toBe('0 bytes');
+    expect(formatBytes(1024)).toBe('1.00 KB');
+    expect(formatBytes(123456789)).toBe('117.74 MB');
+    expect(formatBytes(1024 * 1024 * 1024 * 2)).toBe('2.00 GB');
+  });
+});
