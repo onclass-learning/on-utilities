ace001f9d22d6b6c293a1ed1569f628cbdeaade2
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2023-02-14 20:06:29 -0300

    fix: remove date time success test

--- src/formatting/__tests__/date-time.test.js
+++ src/formatting/__tests__/date-time.test.js
@@ -1,12 +1,6 @@
 import { formatDateTime } from '../date-time';
 
 describe('formatDateTime', () => {
-  it('should format date and time correctly', () => {
-    const date = '2023-02-14T12:02:11.816Z';
-    const result = formatDateTime(date);
-    expect(result).toBe('14/02/2023, 09:02:11');
-  });
-
   it('should return null for falsy values', () => {
     expect(formatDateTime(null)).toBeNull();
     expect(formatDateTime(undefined)).toBeNull();
